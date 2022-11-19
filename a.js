6eda2f04fb1b0fe0d8e2c882bdb9cf07132d6e83
Alvaro Flor <alvaro.gussmao@gmail.com>
2022-11-19 00:47:45 -0300

    feature/1.0.1 pontos

--- a.js
+++ a.js
@@ -1,6 +1,11 @@
 //Jogo par impar
-let mensagem
+let mensagem;
+let pontos;
 function jogoParImpar() {
-    n ? mensagem = "Jogar!" : null
-    return mensagem
+  n ? (mensagem = "Jogar!") : null;
+  return mensagem;
+}
+
+function pontuacao() {
+    pontos++
 }
